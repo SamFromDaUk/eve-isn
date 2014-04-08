81e2a4b1770c2458fb5ebf14c8375c0e2f003c20
Sam Warren <samwarren909@gmail.com>
2014-04-08 22:06:25 +0100

    Base libs and page structure

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="ISN - Incursion Shiny Network">
+
+    <title>ISN - Incursion Shiny Network</title>
+
+    <link rel="stylesheet" type="text/css" href="public/lib/bootstrap-3.1.1-dist/css/bootstrap.css" />
+    <script type="text/javascript" src="public/lib/jquery/jquery-1.11.0.js"></script>
+    <script type="text/javascript" src="public/lib/bootstrap-3.1.1-dist/js/bootstrap.js"></script>
+</head>
+<body>
+
+</body>
+</html>
