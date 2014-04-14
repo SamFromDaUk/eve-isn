1da6a1db014444b4dc8ce731b2add42f10af3cda
Sam Warren <samwarren909@gmail.com>
2014-04-14 21:22:00 +0100

    Updated page about and core

--- page-about/controllers/about_controller.js
+++ page-about/controllers/about_controller.js
@@ -1,7 +1,11 @@
 ISN.Controller.extend('ISN.Controllers.About', {
 
 }, {
+    isnName: ['shiny', 'pve', 'pvp'],
+    nameIndex: 0,
+
     init: function() {
+        this.elements = {};
         this.render();
     },
 
@@ -11,7 +15,33 @@ ISN.Controller.extend('ISN.Controllers.About', {
     },
 
     render: function() {
+        var self = this;
+
         this.element.html(this.view('//page-about/views/about.ejs'));
         this.element.show();
+
+        this.elements.changeable = this.element.find('div.changeable');
+
+        setTimeout(function() {
+            self.swapIsnName();
+        }, 5000);
+    },
+
+    swapIsnName: function() {
+        var self = this;
+
+        if (this.nameIndex >= this.isnName.length) {
+            this.nameIndex = 0;
+        }
+
+        this.elements.changeable.fadeOut(200, function() {
+            self.elements.changeable.text(self.isnName[self.nameIndex]);
+            self.elements.changeable.fadeIn(200);
+        });
+
+        this.nameIndex++;
+        setTimeout(function() {
+            self.swapIsnName();
+        }, 5000);
     }
 });
